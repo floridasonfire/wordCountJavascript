0b7734b78c3acf2119c542a9ecaf4fba319b325e
Jessica Engel & Erin Collins <student@epicodus.com>
2015-05-28 16:17:19 -0700

    list out all words and counts

--- js/scripts.js
+++ js/scripts.js
@@ -1,106 +1,44 @@
-
-var numberToRoman = function(number){
-  var numerals = {1 : "I", 5 : "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
-  var finalArray = []
-
-
-    while (number > 0) {
-      if (number / 1000 >= 1){
-        var timesDivisibleBy = Math.floor(number/1000)
-        for(var i=0; i<timesDivisibleBy; i++){
-          finalArray.push("M");
-        }
-        number = number - (1000*timesDivisibleBy);
-      }
-
-
-      else if (number / 500 >= 1) {
-        var timesDivisibleBy = Math.floor(number/500)
-        if (1000 - number <= 100) {
-          finalArray.push("CM")
-          number = number - 900;
-        } else {
-            for(var i=0; i<timesDivisibleBy; i++) {
-              finalArray.push("D");
-            }
-          number = number - (500*timesDivisibleBy)
-        }
-      }
-
-      else if (number / 100 >= 1) {
-        var timesDivisibleBy = Math.floor(number/100)
-        if (timesDivisibleBy >3) {
-          finalArray.push("CD")
-        } else {
-          for(var i=0; i<timesDivisibleBy; i++) {
-          finalArray.push("C");
-          }
-        }
-        number = number - (100*timesDivisibleBy)
-
-      }
-
-
-      else if (number / 50 >= 1) {
-        var timesDivisibleBy = Math.floor(number/50)
-        if (100 - number <= 10 ) {
-          finalArray.push("XC")
-          number = number - 90;
-        } else {
-          for(var i=0; i<timesDivisibleBy; i++) {
-            finalArray.push("L");
-          }
-          number = number - (50*timesDivisibleBy)
-        }
-      }
-
-
-      else if (number / 10 >= 1) {
-        var timesDivisibleBy = Math.floor(number/10)
-        if (timesDivisibleBy >3) {
-          finalArray.push("XL")
-        } else {
-          for(var i=0; i<timesDivisibleBy; i++) {
-            finalArray.push("X");
-          }
-        }
-        number = number - (10*timesDivisibleBy)
-      }
-
-
-      else if (number / 5 >= 1) {
-        var timesDivisibleBy = Math.floor(number/5)
-        if (number === 9) {
-          finalArray.push("IX")
-          number = number - 9;
-        } else {
-        for(var i=0; i<timesDivisibleBy; i++) {
-          finalArray.push("V");
-        }
-      }
-        number = number - (5*timesDivisibleBy)
-      }
-
-      // ONES
-      else if (number / 1 >= 1) {
-        var timesDivisibleBy = Math.floor(number/1)
-        if (timesDivisibleBy >3) {
-          finalArray.push("IV")
-        } else {
-          for(var i=0; i<timesDivisibleBy; i++) {
-            finalArray.push("I");
-          }
-        }
-        number = number - (1*timesDivisibleBy)
-
-      }
-
-
-
-
-// CLOSING WHILE LOOP
+var removePunctuation = function(string){
+    var regex = new RegExp (/[^0-9a-zA-Z\s]/g)
+    var newString = string.toLowerCase()
+    var puncuation = newString.match(regex)
+    while (puncuation !== null) {
+      newString = newString.replace(regex, "")
+      puncuation = newString.match(regex)
     }
+    return newString;
 
-
-  return finalArray.join("");
 }
+
+var wordCount = function(string) {
+  string = removePunctuation(string);
+  var wordHash = {};
+  var wordArray = string.split(" ")
+  wordArray.forEach(function(word) {
+    if (wordHash[word] === undefined) {
+      wordHash[word] = 1
+    } else {
+      wordHash[word] +=1
+    }
+  })
+  return wordHash;
+}
+
+$(document).ready(function(){
+
+  $("#string-form").submit(function(event){
+    var inputString = $('#string-input').val()
+    console.log(inputString)
+    var resultHash = wordCount(inputString)
+    console.log(resultHash)
+
+    for(var key in resultHash) {
+      $("#results").append([key, "  ", resultHash[key], "<br />"])
+    };
+
+
+    event.preventDefault();
+  })
+
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,81 +1,25 @@
-describe('numberToRoman', function(){
-  it("will return I for 1", function(){
-    expect(numberToRoman(1)).to.equal("I")
+describe('removePunctuation', function(){
+  it("removes punctuation from string", function(){
+    expect(removePunctuation("this! is a string.")).to.equal("this is a string");
   });
-  it("will return V for 5", function(){
-    expect(numberToRoman(5)).to.equal("V");
-  })
+});
 
-  it("will return IV for 4", function(){
-    expect(numberToRoman(4)).to.equal("IV");
-  })
+describe('wordCount', function(){
+  it("returns the count 2 for the string 'word, word'", function(){
+    expect(wordCount("word, word")).to.eql({"word": 2});
+  });
 
-  it("will return IX for 9", function(){
-    expect(numberToRoman(9)).to.equal("IX");
-  })
+  it("returns the count 2 and 1 for the string 'word, word, cat'", function(){
+    expect(wordCount("word, cat, word")).to.eql({"word": 2, "cat": 1});
+  });
 
-  it("will return XL for 40", function(){
-    expect(numberToRoman(40)).to.equal("XL");
-  })
+  it("counts uppercase words and lowercase words as equal", function(){
+    expect(wordCount("word, WORD, cat")).to.eql({"word": 2, "cat": 1});
+  });
 
-  it("will return XLIV for 44", function(){
-    expect(numberToRoman(44)).to.equal("XLIV");
-  })
+  it("counts uppercase words and lowercase words as equal", function(){
+    expect(wordCount("Word, cat, WORD")).to.eql({"word": 2, "cat": 1});
+  });
 
-  it("will return XL for 67", function(){
-    expect(numberToRoman(67)).to.equal("LXVII");
-  })
-
-  it("will return XC for 90", function(){
-    expect(numberToRoman(90)).to.equal("XC");
-  })
-
-  it("will return XCIX for 99", function(){
-    expect(numberToRoman(99)).to.equal("XCIX");
-  })
-
-  it("will return CD for 400", function(){
-    expect(numberToRoman(400)).to.equal("CD");
-  })
-
-  it("will return CDXC for 490", function(){
-    expect(numberToRoman(490)).to.equal("CDXC");
-  })
-
-  it("will return CDXCIX for 499", function(){
-    expect(numberToRoman(499)).to.equal("CDXCIX");
-  })
-
-  it("will return M for 1000", function(){
-    expect(numberToRoman(1000)).to.equal("M");
-  })
-
-  it("will return MCDXCIII for 1493", function(){
-    expect(numberToRoman(1493)).to.equal("MCDXCIII");
-  })
-
-  it("will return MD for 1500", function(){
-    expect(numberToRoman(1500)).to.equal("MD");
-  })
-
-  it("will return MDXXXIII for 1533", function(){
-    expect(numberToRoman(1533)).to.equal("MDXXXIII");
-  })
-
-  it("will return MCMIX for 1909", function(){
-    expect(numberToRoman(1909)).to.equal("MCMIX");
-  })
-
-  it("will return MM for 2000", function(){
-    expect(numberToRoman(2000)).to.equal("MM");
-  })
-
-  it("will return MMMCMIX for 3909", function(){
-    expect(numberToRoman(3909)).to.equal("MMMCMIX");
-  })
-
-  it("will return MMMCMXCIX for 3999", function(){
-    expect(numberToRoman(3999)).to.equal("MMMCMXCIX");
-  })
 
 });
